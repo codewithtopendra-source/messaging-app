527ae1350096c476e18f8dda2146708a6bcb2724
codewithadarsh7 <adarshrana.codes@gmail.com>
2026-08-13 01:24:49 +0545

    update

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,22 +1,15 @@
 "use client"
-import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+import * as React from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import { toast } from "sonner"
+import * as z from "zod"
+
+const page = () => {
   return (
-    <>
-    <div>
-        <p>Not Sign in</p>
-      <button className="bg-orange-500 px-3 py-1 cursor-pointer rounded m-5" onClick={() => signIn()}>Sign in</button>
-    </div>
-    </>
+    <div>page</div>
   )
 }
+
+export default page
