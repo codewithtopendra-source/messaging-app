ff5e568c7dc3983b7cc620c14fa138093ef068c8
codewithadarsh7 <adarshrana.codes@gmail.com>
2026-08-13 23:35:31 +0545

    update

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,6 +10,12 @@ import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios, {AxiosError} from 'axios'
 import { ApiResponse } from "@/types/ApiResponse"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Form } from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import Link from "next/link"
 
 const page = () => {
 
@@ -17,7 +23,7 @@ const page = () => {
   const [usernameMessage, setUsernameMessage] = useState("")
   const [isCheckingUsername, setisCheckingUsername] = useState(false)
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const debouncedUsername = useDebounceValue(username, 300)
+  const [debouncedUsername] = useDebounceValue(username, 300)
   const router = useRouter()
 
   // zod implementation
@@ -51,9 +57,127 @@ const page = () => {
     checkUsernameUnique()
   }, [debouncedUsername])
   
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try{
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      toast.success("Success",{
+        description: response.data.message
+      })
+      router.replace(`/verify/${username}`)
+      setIsSubmitting(false)
+    } catch (error) {
+      console.error("Error in signup of user", error)
+      const axiosError = error as AxiosError<ApiResponse>
+      let errorMessage = axiosError.response?.data.message
+      toast.error("Signup Failed",{
+        description: errorMessage,
+      })
+      setIsSubmitting(false)
+    }
+  }
 
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">
+            Sign up to start your anonymous adventure
+          </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}className="space-y-6">
+            <FieldGroup>
+              <Controller
+                name="username"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="username">Username</FieldLabel>
+                    <Input
+                      {...field}
+                      id="username"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter a username"
+                      autoComplete="off"
+                      onChange={(e) => {
+                        field.onChange(e)
+                        setUsername(e.target.value)
+                      }}
+                    />
+                    {isCheckingUsername && (
+                      <p className="text-sm text-muted-foreground">Checking...</p>
+                    )}
+                    {!isCheckingUsername && usernameMessage && (
+                      <p className="text-sm text-muted-foreground">{usernameMessage}</p>
+                    )}
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      type="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Email"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <Input
+                      {...field}
+                      id="password"
+                      type="password"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Password"
+                      autoComplete="new-password"
+                    />
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+            <Button type="submit" disabled={isSubmitting}>
+              {
+                isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wait
+                  </>
+                ) : ( 'Signup' )
+              }
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{''}
+            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
